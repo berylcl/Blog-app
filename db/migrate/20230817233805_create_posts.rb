25935b934f9d529ff919758e2fa55fe59f3a42df
Gakii Beryl <gakiiberyl@gmail.com>
2023-08-18 03:07:07 +0000

    Making required changes

--- db/migrate/20230817233805_create_posts.rb
+++ db/migrate/20230817233805_create_posts.rb
@@ -1,15 +1,11 @@
-# frozen_string_literal: true
-
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.references :author, null: false, foreign_key: true
+      t.references :author, foreign_key: { to_table: :users }
       t.string :title
       t.text :text
-      t.datetime :created_at
-      t.datetime :updated_at
-      t.integer :comments_counter
-      t.integer :likes_counter
+      t.integer :comments_counter, default: 0
+      t.integer :likes_counter, default: 0
 
       t.timestamps
     end
