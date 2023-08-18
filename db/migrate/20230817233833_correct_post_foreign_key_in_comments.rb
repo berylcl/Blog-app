6e1af5d62a431ee1f477fd922d3e40c8b61461e0
Gakii Beryl <gakiiberyl@gmail.com>
2023-08-18 01:58:54 +0000

    add foreign key in comments and database.yml

--- db/migrate/20230817233833_correct_post_foreign_key_in_comments.rb
+++ db/migrate/20230817233833_correct_post_foreign_key_in_comments.rb
@@ -1,7 +1,7 @@
 class CorrectPostForeignKeyInComments < ActiveRecord::Migration[7.0]
   def change
     change_table :comments do |t|
-      t.remove :post
+      t.remove :post_id
       t.references :post, foreign_key: { to_table: :posts }
     end
   end
